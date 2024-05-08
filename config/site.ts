34d8e682cb143ab311fa19a4fa1bff1267aca980
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-05-07 22:18:51 -0400

    feat: site config

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "Color Matrix",
+  author: "Jaleel Bennett",
+  description: `A color matching game built with Next.js, Zustand and Shadcn UI.`,
+  ogImage: "",
+  url: "",
+  links: {
+    twitter: "https://twitter.com/jal_eelll",
+    github: "https://github.com/JaleelB/color-matrix",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
