9554f48e85ad52b76f3b59a63ecb590ef32f2974
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-05-08 21:10:14 -0400

    refactor: passing dialog trigger and footer action as props

--- components/game/how-to-play.tsx
+++ components/game/how-to-play.tsx
@@ -1,6 +1,5 @@
 import {
   AlertDialog,
-  AlertDialogAction,
   AlertDialogContent,
   AlertDialogDescription,
   AlertDialogFooter,
@@ -8,42 +7,24 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
-import { Button } from "../ui/button";
-import { useGameState } from "@/state/game";
 
-export const HowToPlay: React.FC = () => {
-  const updateGameStatus = useGameState((state) => state.setGameStatus);
+interface HowToPlayProps {
+  trigger: React.ReactNode;
+  footerAction: React.ReactNode;
+}
 
+export const HowToPlay: React.FC<HowToPlayProps> = ({
+  trigger,
+  footerAction,
+}) => {
   return (
     <AlertDialog>
-      <AlertDialogTrigger asChild>
-        <Button
-          variant="link"
-          size="icon"
-          className="text-white"
-          onClick={() => updateGameStatus("paused")}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="28"
-            height="28"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            className="lucide lucide-circle-help"
-          >
-            <circle cx="12" cy="12" r="10"></circle>
-            <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path>
-            <path d="M12 17h.01"></path>
-          </svg>
-        </Button>
-      </AlertDialogTrigger>
+      <AlertDialogTrigger asChild>{trigger}</AlertDialogTrigger>
       <AlertDialogContent>
         <AlertDialogHeader className="space-y-4">
-          <AlertDialogTitle>How to Play Color Matrix?</AlertDialogTitle>
+          <AlertDialogTitle className="text-accent-secondary">
+            How to Play Color Matrix?
+          </AlertDialogTitle>
           <div className="w-full min-h-[200px] border grid place-content-center">
             gameplay gif
           </div>
@@ -52,7 +33,9 @@ export const HowToPlay: React.FC = () => {
             and color recognition skills. Here&apos;s how you can master the
             game and beat the high scores!
             <div className="w-full mt-4">
-              <h3 className="font-bold mb-2 text-white">Objective: </h3>
+              <h3 className="font-bold mb-2 text-accent-secondary">
+                Objective:{" "}
+              </h3>
               <p>
                 Your goal is to correctly identify and select tiles that match
                 the color name displayed within the time limit. Each correct
@@ -61,7 +44,9 @@ export const HowToPlay: React.FC = () => {
               </p>
             </div>
             <div className="w-full mt-4">
-              <h3 className="font-bold mb-2 text-white">Tips for Success </h3>
+              <h3 className="font-bold mb-2 text-accent-secondary">
+                Tips for Success{" "}
+              </h3>
               <ol className="w-full space-y-2 list-outside">
                 <li>
                   Stay Focused: The colors and text can be misleading. Focus on
@@ -75,11 +60,7 @@ export const HowToPlay: React.FC = () => {
             </div>
           </AlertDialogDescription>
         </AlertDialogHeader>
-        <AlertDialogFooter className="mt-4">
-          <AlertDialogAction onClick={() => updateGameStatus("running")}>
-            Continue
-          </AlertDialogAction>
-        </AlertDialogFooter>
+        <AlertDialogFooter className="mt-4">{footerAction}</AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
   );
--- components/views/game.tsx
+++ components/views/game.tsx
@@ -6,6 +6,8 @@ import Link from "next/link";
 import { Pause } from ".";
 import { HowToPlay } from "../game/how-to-play";
 import { GameGrid } from "../game/game-grid";
+import { Button } from "../ui/button";
+import { AlertDialogAction } from "../ui/alert-dialog";
 
 const GameHeader: React.FC = () => {
   const score = useGameState((state) => state.currentScore);
@@ -21,10 +23,10 @@ const GameHeader: React.FC = () => {
 };
 
 const GameFooter: React.FC = () => {
-  const { gameLevel, highScore } = useGameState((state) => ({
-    gameLevel: state.gameLevel,
-    highScore: state.highScore,
-  }));
+  const gameLevel = useGameState((state) => state.gameLevel);
+  const highScore = useGameState((state) => state.highScore);
+  const updateGameStatus = useGameState((state) => state.setGameStatus);
+
   return (
     <footer className="fixed bottom-0 left-0 w-full h-20 text-white flex justify-between items-center px-4 lg:px-8">
       {/* <Timer /> */}
@@ -39,7 +41,41 @@ const GameFooter: React.FC = () => {
       </Link>
       <div className="flex gap-4 items-end">
         <div className="flex">
-          <HowToPlay />
+          <HowToPlay
+            trigger={
+              <Button
+                variant="link"
+                size="icon"
+                className="text-white"
+                onClick={() => updateGameStatus("paused")}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="28"
+                  height="28"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="lucide lucide-circle-help"
+                >
+                  <circle cx="12" cy="12" r="10"></circle>
+                  <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path>
+                  <path d="M12 17h.01"></path>
+                </svg>
+              </Button>
+            }
+            footerAction={
+              <AlertDialogAction
+                className="bg-accent-secondary border-accent-secondary hover:bg-accent-secondary"
+                onClick={() => updateGameStatus("running")}
+              >
+                Continue
+              </AlertDialogAction>
+            }
+          />
           <Pause />
         </div>
         <div className="text-right flex flex-col">
