1299efd9543936fbab05043a3b28f0fc88f85d8d
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-05-12 16:41:23 -0400

    feat: wrapping app config in pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
-import MillionLint from '@million/lint';
+import MillionLint from "@million/lint";
+import withPWAInit from "@ducanh2912/next-pwa";
 /** @type {import('next').NextConfig} */
+
+const withPWA = withPWAInit({
+  dest: "public",
+});
+
 const nextConfig = {};
-export default MillionLint.next({
-  rsc: true
+export default withPWA({
+  rsc: true,
 })(nextConfig);
