930787c11c1776006fa0650258e8039d3a67b172
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-05-12 14:14:46 -0400

    chore: site metadata object

--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,8 @@
 export const siteConfig = {
   name: "Color Matrix",
   author: "Jaleel Bennett",
-  description: `A color matching game built with Next.js, Zustand and Shadcn UI.`,
-  ogImage: "",
-  url: "",
+  description: `A color matching game designed to challenge and enhance your cognitive skills through the Stroop effect`,
+  url: "https://colormatrix.vercel.app/",
   links: {
     twitter: "https://twitter.com/jal_eelll",
     github: "https://github.com/JaleelB/color-matrix",
