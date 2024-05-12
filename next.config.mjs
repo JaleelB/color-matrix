b0deb36744a8e664996407051dbb31f968d758a6
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-05-12 17:12:04 -0400

    fix: ignoring autogenerated pwa files

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,19 @@
-import MillionLint from "@million/lint";
 import withPWAInit from "@ducanh2912/next-pwa";
 /** @type {import('next').NextConfig} */
 
 const withPWA = withPWAInit({
   dest: "public",
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  register: true, // Register the PWA service worker
+  skipWaiting: true,
+  rsc: true,
 });
 
-const nextConfig = {};
-export default withPWA({
-  rsc: true,
-})(nextConfig);
+const nextConfig = {
+  reactStrictMode: true, // Enable React strict mode for improved error handling
+  swcMinify: true, // Enable SWC minification for faster builds
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+  },
+};
+export default withPWA(nextConfig);
